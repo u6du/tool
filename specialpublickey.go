a6af5c7e0b304f91c7e68d68911c0a42cc1467ae
- <->
2019-06-29 09:22:21 +0800

    .

--- specialpublickey.go
+++ specialpublickey.go
@@ -4,26 +4,20 @@ package main
 import (
 	"crypto/rand"
 	"io/ioutil"
+	"runtime"
 	"strings"
 
-	"github.com/rs/zerolog/log"
 	"github.com/u6du/ex"
 	"github.com/u6du/go-rfc1924/base85"
 	"golang.org/x/crypto/ed25519"
 )
 
-func main() {
-	count := 0
+func run(ch chan string) {
 NEXT:
 	for {
 		_, private, err := ed25519.GenerateKey(rand.Reader)
 		ex.Panic(err)
-		count++
-		if count%100000 == 0 {
-			log.Info().Int("count", count).Msg("")
-		}
 		public := base85.EncodeToString(private.Public().(ed25519.PublicKey))
-
 		if strings.Index(strings.ToLower(public), "6du") != 0 {
 			continue
 		}
@@ -34,10 +28,20 @@ NEXT:
 			}
 		}
 
-		println(public)
-
 		filepath := "6du.private"
 		ex.Panic(ioutil.WriteFile(filepath, private.Seed(), 0600))
+
+		ch <- public
 		break
 	}
+}
+
+func main() {
+	ch := make(chan string)
+	num := runtime.GOMAXPROCS(runtime.NumCPU())
+	for i := 0; i < num; i++ {
+		go run(ch)
+	}
+	public := <-ch
+	println(public)
 }
