7e38e82b96f3cb4918145a5d975a6420aba53f1c
- <->
2019-07-04 12:44:14 +0800

    -

--- specialpublickey/run.sh
+++ specialpublickey/run.sh
@@ -4,4 +4,6 @@ _dirname=$(cd "$(dirname "$0")"; pwd)
 
 cd $_dirname
  
-nohup docker run --rm -v $_dirname:$_dirname -w $_dirname u6du/cloudflare-dns go run specialpublickey.go &
+nohup docker run --rm \
+-v /root/go:/go \
+-v $_dirname:$_dirname -w $_dirname u6du/cloudflare-dns go run main.go &
