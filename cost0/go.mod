e190f6ea2b78a20e76c4fa462ec71b13f7231519
- <->
2019-07-12 22:13:13 +0800

    .

--- cost0/go.mod
+++ cost0/go.mod
@@ -2,4 +2,7 @@ module cost0
 
 go 1.12
 
-require golang.org/x/crypto v0.0.0-20190701094942-4def268fd1a4 // indirect
+require (
+	github.com/u6du/zerolog v1.15.6 // indirect
+	golang.org/x/crypto v0.0.0-20190701094942-4def268fd1a4 // indirect
+)
--- cost0/main.go
+++ cost0/main.go
@@ -3,13 +3,52 @@ package main
 
 import (
 	"fmt"
+	"time"
 
 	"golang.org/x/crypto/blake2b"
 )
 
+func next(msg []byte) []byte {
+	for i := range msg {
+		t := msg[i]
+		if t != 255 {
+			msg[i] = t + 1
+			for j := 0; j < i; j++ {
+				msg[j] = 0
+			}
+			return msg
+		}
+	}
+	return make([]byte, len(msg)+1)
+}
+
+func Begin0Count(msg [32]byte) (n uint) {
+
+	for i := range msg {
+		if msg[i] == 0 {
+			n++
+		} else {
+			break
+		}
+	}
+
+	return
+}
+
 func main() {
 	var msg []byte
+	atLest := uint(2)
+	begin := uint64(time.Now().UnixNano())
+	count := uint(0)
 
-	msg = append(msg, byte(0))
-	fmt.Println("vim-go", blake2b.Sum256(msg))
+	for {
+		msg = next(msg)
+		hash := blake2b.Sum256(msg)
+		if Begin0Count(hash) > atLest {
+			count += 1
+			cost := (uint64(time.Now().UnixNano()) - begin)/uint64(time.Millisecond)/uint64(count)
+			fmt.Printf("%dms/hash %d %d msg %x hash %x\n", cost, count, len(msg), msg, hash)
+		}
+
+	}
 }
